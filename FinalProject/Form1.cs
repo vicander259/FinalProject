76662a9a6b1b9ad5958108dd09ba8b82c9628694
Victor Anderson <victor.anderson259@gmail.com>
2015-11-09 10:50:01 -0800

    upload commit

---
+++ FinalProject/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string word1, word2, word3, word4;
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            word1 = textBox1.Text;
+            word2 = textBox2.Text;
+            word3 = textBox3.Text;
+            word4 = textBox4.Text;
+            Form2 myForm = new Form2();
+            myForm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ FinalProject/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {           
+            InitializeComponent();
+        }
+             
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string word1 = Form1.word1;
+            int word1Length = word1.Length;
+            label3.Text = word1Length.ToString();
+        }
+    }
+}
